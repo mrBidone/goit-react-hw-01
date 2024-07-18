4d7fcbd598226b80c18b290d9353ae9a297905ae
mrBidone <ab.bz@ukr.net>
2024-07-19 00:11:24 +0200

    Created project with structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,34 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import "modern-normalize";
+import viteLogo from '/vite.svg'
+import '../components/App.css'
 
-import './App.css'
+const userData = {
+  username: "Jacques Gluke",
+  tag: "jgluke",
+  location: "Ocho Rios, Jamaica",
+  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308
+  }
+};
 
-import Product from './Product';
 
-export default function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Products</h1>
-      <Product name="Taco with lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={12.99}/>
-      <Product name="Taco with beef"
-      imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={20.99}/>
-      <Product name="Taco with chees"
-      imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={30.99}/>
-    </div>
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
   );
-}
+};
 
-
+export default App
