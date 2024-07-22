f8dba084d64804dba99b225a5227f11893b0371b
mrBidone <ab.bz@ukr.net>
2024-07-22 14:41:43 +0200

    check upd

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App'
+import App from './App.jsx'
 import './index.css'
-import Profile from './components/Profile/Profile'
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Profile />
+    <App />
   </React.StrictMode>,
 )
